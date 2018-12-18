6e7e4e793a7dc9a3c545e63a6760f22d82d87362
KaloshinPE <pavel.kaloshin@phystech.edu>
2018-12-19 01:42:04 +0300

    added kernel

--- nn/layers.py
+++ nn/layers.py
@@ -1,4 +1,4 @@
-import torch 
+import torch
 from torch import nn
 
 
@@ -13,19 +13,19 @@ class SpectralConv(nn.Module):
         self.short_scales = short_scales
         self.long_scales = long_scales
         self.mlp = nn.Linear(k, k)
-        
+
     def forward(self, X, S, V, R):
         '''
         X - featurs
         S - affinity matrix
-        V - Q@B - Q from Lanczos, B from eigendecomposition 
+        V - Q@B - Q from Lanczos, B from eigendecomposition
         R - Ritz values
         '''
         Y = X
         Z = Y
         features = []
         for l in range(1, self.short_scales[-1] + 1):
-            Z = torch.mm(S,Z)
+            Z = torch.mm(S, Z)
             if l in self.short_scales:
                 features.append(Z)
 
@@ -35,5 +35,39 @@ class SpectralConv(nn.Module):
             Z = torch.mm(Z, V.t())
             Z = torch.mm(Z, Y)
             features.append(Z)
-            
+
         return torch.cat(features, 1)
+
+
+class IdentityModule(nn.Module):
+    def forward(self, inputs):
+        return inputs
+
+
+class ExpKernel(nn.Module):
+    def __init__(self, X, layers=None, e=10):
+        super().__init__()
+        self.input_features = X
+        self.mlp = []
+        shape = X.shape[-1]
+
+        if layers is not None:
+            for layer in layers:
+                self.mlp.append(nn.modules.Linear(shape, layer))
+                self.mlp.append(nn.modules.ReLU())
+                shape = layer
+        if len(self.mlp) > 0:
+            self.mlp = nn.Sequential(*self.mlp[:-1])
+        else:
+            self.mlp = IdentityModule()
+
+        self.e = e
+
+    def forward(self, input):
+        Y = self.mlp(input)
+        n = Y.size(0)
+        norms = torch.sum(Y**2, dim=1, keepdim=True)
+        norms_squares = (norms.expand(n, n) + norms.t().expand(n, n))
+        distances_squared = torch.sqrt(1e-6 + norms_squares - 2 * Y.mm(Y.t()))
+        A = torch.exp(-distances_squared/self.e)
+        return A
